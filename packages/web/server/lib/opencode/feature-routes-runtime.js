a844c9fa055462fc6f1fb407f82180c4e7a6b582
binary1215 <binary1215@users.noreply.github.com>
2026-04-03 15:41:06 +0900

    fix(server): wire runtime-managed provider connect route

--- packages/web/server/lib/opencode/feature-routes-runtime.js
+++ packages/web/server/lib/opencode/feature-routes-runtime.js
@@ -58,7 +58,7 @@ export const createFeatureRoutesRuntime = (dependencies) => {
       runtimeRolloutConfig,
     } = routeDependencies;
 
-    const { getProviderSources, removeProviderConfig } = await import('./index.js');
+    const { getProviderSources, removeProviderConfig, upsertProviderConfig } = await import('./index.js');
 
     registerSettingsUtilityRoutes(app, {
       readCustomThemesFromDisk,
@@ -81,6 +81,7 @@ export const createFeatureRoutesRuntime = (dependencies) => {
       resolveProjectDirectory,
       getProviderSources,
       removeProviderConfig,
+      upsertProviderConfig,
       refreshOpenCodeAfterConfigChange,
     });
 
